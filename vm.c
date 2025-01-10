8e416c214bdd04bd4c8a884318efd5a9f0a1b83f
renearevalo13 <reneorlandoarevalo@gmail.com>
2025-01-09 22:29:29 -0800

    created structures and have a constructor for the virtual machine

--- vm.c
+++ vm.c
@@ -1,7 +1,81 @@
-/* vm */
+/* vm.c */
 
 #include "vm.h"
+#include "birchutils.h"
 
-int main(int argc, char *argv[]) {
+VM *virtualmachine(Program pr, int16 progsz) {
+    VM *p;
+    int16 size;
+
+    assert((pr) && (progsz));
+
+    Program pp;
+
+    size = $2 sizeof(struct s_vm);
+    p = (VM *)malloc($i size);
+    if (!p) {
+        errno = ErrMem;
+        return (VM *)0;
+    }
+
+    zero($1 p, size);
+    pp = (Program)malloc($i progsz);
+    if (!pp) {
+        free(p);
+        errno = ErrMem;
+        return (VM *)0;
+    }
+    copy($1 pp, $1 pr, progsz);
+    return p;
+
+}
+
+int8 map(Opcode o){
+    int8 n, ret;
+    IM *p;
+
+    ret = 0;
+    for (n=IMs, p=instrmap; n; n--, p++) {
+        if (p->o == o) {
+            ret = p->s;
+            break;
+        }
+    }
+    return ret; 
+}
+
+Program exampleprogram() {
+
+    int8 size;
+    //Program prog[2];
+    Instruction i1, i2;
+
+    size = map(mov);
+    i1 = (Instruction)malloc($i size);
+    if (!i1) {
+        errno = ErrMem;
+        return (Program)0;
+    }
+
+    size = map(nop);
+    i2 = (Instruction)malloc($i size);
+    if (!i2) {
+        errno = ErrMem;
+        return (Program)0;
+    }
+    Program prog = {i1, i2}; 
+    return prog;
+}
+
+int main (int argc, char *argv[]){
+    Program prog;
+    VM *vm;
+    int8 size;
+    size = map(mov) + map(nop);
+
+    prog = exampleprogram();
+    printf("prog = %p\n", prog);
+    vm = virtualmachine(prog, size);
+    printf("vm = %p\n", vm);
     return 0;
 }
--- vm.h
+++ vm.h
@@ -8,20 +8,91 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <errno.h>
-#include <birchutils.h>
+#include "birchutils.h"
+
+#define ErrMem 0x01
+#define NoArgs {0x00, 0x00}
 
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 typedef unsigned long long int int64;
 
-#define $8 (int8 *)
-#define $6 (int16)
-#define $2 (int32)
-#define $4 (int64)
+#define $1 (int8 *)
+#define $2 (int16)
+#define $4 (int32)
+#define $8 (int64)
 #define $c (char *)
 #define $i (int)
 
+/*
+   Will be making a 16 bit computer.
+   16 bits gives us 65,536 memory addresses.
+   65kb of memory.
+
+   CPU will be simple.
+   Ax, Bx, Cx, Dx registers.
+   Stack pointer, sp.
+   Instruction pointer, ip.
+   other ports to come.
+*/
+typedef unsigned short int Reg;
+
+struct s_registers {
+    Reg ax;
+    Reg bx;
+    Reg cx;
+    Reg dx;
+    Reg sp;
+    Reg ip;
+};
+typedef struct s_registers Registers;
+
+enum e_opcode {
+    mov = 0x01,
+    nop = 0x02
+};
+typedef enum e_opcode Opcode;
+
+struct s_instrmap {
+    Opcode o;
+    int8 s;
+};
+typedef struct s_instrmap IM;
+
+struct s_cpu {
+    Registers r;
+};
+typedef struct s_cpu CPU;
+
+typedef int8 Args;
+
+struct s_instruction {
+    Opcode o;
+    Args a[];
+};
+typedef struct s_instruction *Instruction;
+
+typedef int8 Stack[((unsigned int)(-1))];
+typedef Instruction Program;
+
+struct s_vm {
+    CPU c;
+    Stack *s;
+    Program *p;
+};
+typedef struct s_vm VM;
+
+static IM instrmap[] = {
+    {mov, 0x03},
+    {nop, 0x01}
+};
+#define IMs sizeof(instrmap) / sizeof(struct s_instrmap)
+ Program exampleprogram(void);
+
+int8 map(Opcode); 
+VM *virtualmachine (Program , int16);
 
 int main (int, char**);
+
 
